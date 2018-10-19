c62816e5166889d66915cceb38fb5c42f8cd2066
lulululululululu <limenglu0219@126.com>
2018-10-19 16:37:15 -0400

    Update server.py

--- server.py
+++ server.py
@@ -53,7 +53,7 @@ def login_step2():
     user_email = request.forms.get('email')
     session = bottle.request.environ.get('beaker.session')
     if  user_email not in session:
-        flow = flow_from_clientsecrets("client_secret_236907109154-1us7tahjvssjcmtfk3orivipcjr4lult.apps.googleusercontent.com.json",scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email', redirect_uri="http://localhost:8000/redirect")
+        flow = flow_from_clientsecrets("client_secret_236907109154-1us7tahjvssjcmtfk3orivipcjr4lult.apps.googleusercontent.com.json",scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email', redirect_uri="http://localhost:8080/redirect")
         auth_uri = flow.step1_get_authorize_url()
         bottle.redirect(str(auth_uri))
     else:
