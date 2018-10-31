ec176912f87b2c54c914012165306af0c2e7b86a
jiehao <jiehao.liu@mail.utoronto.ca>
2018-10-31 18:11:04 -0400

    lab3

--- index.tpl
+++ index.tpl
@@ -6,6 +6,18 @@
 	    text-align: left;
 	    padding: 8px;
 	}
+	
+	ul {
+	  list-style-type: none;
+	}
+	
+	ul li{
+		display:inline;
+	}
+	ul li input[type=submit] {
+	    height: 20em;
+		font-size:10em;
+	}
 </style>
 <body>
 % if user_email:
@@ -33,57 +45,93 @@
 	
 <!--show the results if keywords entered -->
 % if keywords: 
-	<h1 align="center">Search for "{{keywords}}"</h1>
+%	word = keywords.lower().split()[0]
+	<h1 align="center">Search for "{{word}}"</h1>
 
 <!--count is a dict storing each keyword and its number of occurence, it is used in result table-->
 <!--temp dict is used in the case where there are already 20 keywords stored in the record dict, those new keywords are temporarily stored in temp and will be used to compare with the current top 20 popular keywords. Keywords having larger number of occurence will be put or remained in record dict-->
 <%
 	count = {}  
 	
-	for word in keywords.lower().split():
-		if word not in count:
-			count[word]=1
-		else:
-			count[word]+=1
-		end
+	if word not in count:
+		count[word]=1
+	else:
+		count[word]+=1
+	end
 		
-		if user_email:
-			if word not in record:
-				if len(record) < 10:
-					record[word]=1
-				else: 
-					record.popitem(last=False)	
-					record[word]=1			
-				end
-			else:
-				temp = record[word]
-				del record[word]
-				record[word]= temp+1
+	if user_email:
+		if word not in record:
+			if len(record) < 10:
+				record[word]=1
+			else: 
+				record.popitem(last=False)	
+				record[word]=1			
 			end
+		else:
+			temp = record[word]
+			del record[word]
+			record[word]= temp+1
 		end
 	end
-	
-	
-	
-	
-	
+
+%>
+<% if word in resolved_inverted_index:
+      urls = resolved_inverted_index[word]
+	  ranked_url = {}
+	  for url in urls:
+	  	 url_id = lexicon[url]
+		 url_score = page_rank[str(url_id)]
+		 ranked_url[url_score] = url
+	  end
+	  
+	  ranked_url = sorted(ranked_url.iteritems())	
+	  num_of_page = len(ranked_url)/5+1     
+	  curr_page = int(page_num)
 %>
 	<table id=”results” style="margin:auto">
 		<tr style="font-size:1.5em">
-			<th>Word</th>
-			<th>Count</th>
+			<th>No</th>
+			<th>Urls</th>
 		</tr>
-% for key in keywords.lower().split():
-	% if key in count:
+<%
+	start_index = 5*(curr_page-1)
+	end_index = 5*curr_page
+	counter = start_index
+	ranked_url = list(reversed(ranked_url))
+	for url_score, url in ranked_url[start_index:end_index]:
+		counter+=1
+		print url_score
+
+%>
 		<tr style="font-size:1.5em">
-			<td style="padding-left:1.5em">{{key}}</td>
-			<td align="center">{{count[key]}}</td>
+			<td style="padding-left:0.5em">{{counter}}</td>
+			<td style="padding-left:0.5em"><a href="{{url}}">{{url}}</a></td>
 		</tr>
-	% del count[key]
-	% end
-% end
+    % end
+	
 	</table>
-% end
+	<form action="/change_page" method="post">
+	<nav align="center">
+		<ul>
+	% if curr_page > 1:
+		<li ><input name="page_num" type="submit" value="prev" /></li>
+	% end
+	% for page_num in range(1,num_of_page):
+	% 	if page_num == curr_page:
+		<li ><input style="color:blue;" name="page_num" type="submit" value="{{page_num}}" /></li>
+	%   else:
+	    <li ><input name="page_num" type="submit" value="{{page_num}}" /></li>
+	%   end
+	% end
+	% if curr_page < num_of_page-1:
+		<li ><input name="page_num" type="submit" value="next" /></li>
+	% end
+		</ul>
+	</nav>
+	</form>
+% 	else:
+		<div align="center" style="font-size:1.5em">No results found </div>	
+% 	end		
 
 <!--show top 20 most popular keywords if any record exists -->
 % if len(record)>0 and user_email:
--- server.py
+++ server.py
@@ -7,18 +7,36 @@ from googleapiclient.discovery import build
 import httplib2
 from beaker.middleware import SessionMiddleware
 from collections import OrderedDict
-
-
+import redis
+import json
+from bottle import error
+    
 # the record dict is used to store top 20 most popular keywords
 record = OrderedDict()
 user_email = ''
+page_num = 1
+keywords = ""
 
+# lab2
 session_opts = {
     'session.type': 'memory',
     'session.cookie_expires': True,
     'session.auto': True
 }
 app = SessionMiddleware(bottle.app(), session_opts)
+
+# lab3
+redis_db = redis.StrictRedis(host="localhost", port=6379, db=0) #connect to redis database
+page_rank = redis_db.get('page_rank') #page rank result - page_rank[url_id] = score
+inverted_index = redis_db.get('inverted_index') # word_id & its set of url_id -inverted_index[word_id]=set_of(url_ids)
+resolved_inverted_index =redis_db.get('resolved_inverted_index') # word & its set of url-resolved_inverted_index[word]=set_of(urls)
+lexicon = redis_db.get('url_id_lexicon') # lexicon[url]=url_id
+
+# convert data from str to dict
+page_rank = json.loads(page_rank)
+inverted_index = json.loads(inverted_index)
+resolved_inverted_index = json.loads(resolved_inverted_index)
+lexicon = json.loads(lexicon)
 
 def getUserEmail(credentials):
     token = credentials.id_token['sub']
@@ -31,7 +49,23 @@ def getUserEmail(credentials):
     user_document = users_service.userinfo().get().execute() 
     user_email = user_document['email']
     return user_email
+ 
+@error(404)
+def error404(error):
+    return template('error') 
     
+@route('/change_page',method='POST')
+def decide_page_num():
+    global page_num,keywords
+    new_page_num = request.forms.get('page_num')
+    if new_page_num == "prev":
+        page_num = int(page_num)-1
+    elif new_page_num == "next":
+        page_num = int(page_num)+1
+    else:
+        page_num = int(new_page_num)     
+    return template('index',keywords=keywords,record=record, user_email=user_email,resolved_inverted_index=resolved_inverted_index, lexicon=lexicon,page_rank=page_rank,page_num=page_num)
+          
 @route('/',method='GET')
 def index():
     global user_email,record
@@ -46,6 +80,7 @@ def index():
 @route('/login_step1',method='POST')
 def login_step1():
     return template('login')
+    
 @route('/login_step2',method='POST')
 def login_step2():
     global user_email,record
@@ -74,7 +109,7 @@ def redirect_page():
     global user_email,record
     CLIENT_ID = '236907109154-1us7tahjvssjcmtfk3orivipcjr4lult.apps.googleusercontent.com'
     CLIENT_SECRET = 'mvAagtXMQyUTMS0EMkTRhsOM'
-    SCOPE = 'https://www.googleapis.com/auth/calendar'
+    SCOPE = 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email'
     REDIRECT_URI = 'http://localhost:80/redirect'
     code = request.query.get('code', '')
     flow = OAuth2WebServerFlow(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,scope=SCOPE,redirect_uri=REDIRECT_URI)        
@@ -92,13 +127,14 @@ def server_static(filename):
     
 @route('/',method='POST')
 def result():
-    global user_email,record
+    global user_email,record,keywords,page_num
     session = bottle.request.environ.get('beaker.session')
     if user_email in session:
         session[user_email]=record
     else:
         record = OrderedDict()
     keywords = request.forms.get('keywords')
-    return template('index',keywords=keywords,record=record, user_email=user_email)
+    page_num = 1
+    return template('index',keywords=keywords,record=record, user_email=user_email,resolved_inverted_index=resolved_inverted_index, lexicon=lexicon,page_rank=page_rank,page_num=page_num)
 
-run(app=app, host='172.31.43.73', port=80)
+run(app=app, host='0.0.0.0', port=8080)
